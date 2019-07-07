bec7bfee6f1e9629a19954b44b5fba086b368c5b
Chico27 <42705959+Chico27@users.noreply.github.com>
2019-07-07 18:47:07 +0900

    new

---
+++ backup/backup1.js
@@ -0,0 +1,24 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import "./styles.css";
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <h1>Hello CodeSandbox</h1>
+//       <h2>Start editing to see some magic happen!</h2>
+//     </div>
+//   );
+// }
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+  <div>
+    <h1>Hello World!</h1>
+    <p>This is paragraph</p>
+  </div>,
+  rootElement
+);
+
+var myNewP = document.createElement("p");
+myNewP.innerHTML = "This is a paragraph";
