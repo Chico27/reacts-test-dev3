2cdf3f3e6073cbc6b0c4a7e8b5b16f1f16002b89
Chico27 <42705959+Chico27@users.noreply.github.com>
2019-07-14 12:37:25 +0900

    new

--- src/index.js
+++ src/index.js
@@ -2,27 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import "./styles.css";
-
-function App() {
-  const date = new Date();
-  const hours = date.getHours();
-  let timeOfDay;
-
-  if (hours < 12) {
-    timeOfDay = "morning";
-  } else if (hours >= 12 && hours < 17) {
-    timeOfDay = "afternoon";
-  } else {
-    timeOfDay = "night";
-  }
-
-  const styles = {
-    color: "#FF8008",
-    backgroundColor: "#FF2D00",
-    fontSize: "200px"
-  };
-
-  return <h1 style={styles}>Good {timeOfDay}</h1>;
-}
+import App from "./App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
